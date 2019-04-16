0d1612659980cddcddba52b12a49eb960125c21b
Xavier Cornwell <xaviercornwell@yahoo.com>
2019-04-15 21:47:41 -0700

    submit

---
+++ cookie_eat.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+<?php
+    echo '<p>' . $_COOKIE['username'] . '</p>';
+    echo '<p>' . $_COOKIE['firstname'] . '</p>';
+    echo '<p>' . $_COOKIE['lastname'] . '</p>';
+
+    if(isset($_COOKIE["cookie"])){
+        foreach($_COOKIE["cookie"] as $key=> $val){
+            echo $key . ' is ' . $val . "<br>\n";
+        }
+
+    }
+
+?>
+</body>
+</html>
